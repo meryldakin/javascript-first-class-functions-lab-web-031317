76b722aa7986bfc8a0fab30f45bfc81f02b88464
meryldakin <meryl.k.dakin@gmail.com>
2017-04-25 14:39:53 -0400

    Done.

---
+++ index.js
@@ -0,0 +1,21 @@
+function countdown(callback) {
+  window.setTimeout( callback, 2000 )
+}
+
+
+function createMultiplier(multiplierValue) {
+  return function(num) {
+    return multiplierValue * num
+  }
+}
+
+var doubler = createMultiplier(2)
+var tripler = createMultiplier(3)
+
+
+function multiplier(multiplierValue, value) {
+  return multiplierValue * value
+}
+
+var doublerWithBind = multiplier.bind(null, 2)
+var triplerWithBind = multiplier.bind(null, 3)
